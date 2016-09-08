4e65ded32ba3755a61ce7652ae707936eaf79a35
odinhaus <bill@floggi.com>
2016-09-08 14:50:06 -0500

    include checking manually added serializers

--- Altus.Suffūz/Serialization/SerializationContext.cs
+++ Altus.Suffūz/Serialization/SerializationContext.cs
@@ -58,6 +58,7 @@ namespace Altus.Suffūz.Serialization
                 else
                 {
                     ISerializer serializer = App.ResolveAll<ISerializer>()
+                        .Union(_serializers.Values)
                         .Where(s => s.SupportsFormat(format) && s.SupportsType(type))
                         .OrderBy(s => s.Priority)
                         .FirstOrDefault();
